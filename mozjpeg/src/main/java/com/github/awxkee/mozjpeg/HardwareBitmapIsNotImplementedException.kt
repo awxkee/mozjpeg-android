dd6f55b4fd2c149d380c499946e73e2d00fcd5a5
awxkee <qnljoy@gmail.com>
2023-01-01 23:31:19 +0700

    Added rejection for hardware bitmaps

--- mozjpeg/src/main/java/com/github/awxkee/mozjpeg/HardwareBitmapIsNotImplementedException.kt
+++ mozjpeg/src/main/java/com/github/awxkee/mozjpeg/HardwareBitmapIsNotImplementedException.kt
@@ -3,5 +3,5 @@ package com.github.awxkee.mozjpeg
 import androidx.annotation.Keep
 
 @Keep
-class HardwareBitmapIsNotImplementedException: Exception("Hardware bitmaps currently are not implemented") {
-}
+class HardwareBitmapIsNotImplementedException :
+    Exception("Hardware bitmaps currently are not implemented")
